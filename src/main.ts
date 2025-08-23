1be95b271bbad7c78bef4f359c115cfae991544c
João Mateus <joaomgsb@gmail.com>
2025-08-23 02:36:19 -0300

    swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import "reflect-metadata";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { writeFileSync } from "fs"; // Importe o fs diretamente no topo
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,9 +16,14 @@ async function bootstrap() {
     const doc = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup("docs", app, doc);
 
+    // Se a variável para escrever o arquivo estiver ativa...
     if (process.env.SWAGGER_WRITE === "true") {
-      const fs = await import("node:fs");
-      fs.writeFileSync("openapi.json", JSON.stringify(doc, null, 2));
+      // Escreve o arquivo...
+      writeFileSync("openapi.json", JSON.stringify(doc, null, 2));
+
+      // E AGORA A PARTE IMPORTANTE:
+      await app.close(); // 1. Desliga a aplicação
+      return;            // 2. Encerra a função bootstrap para não executar o app.listen()
     }
   }
 
